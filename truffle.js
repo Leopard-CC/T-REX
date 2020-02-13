448edae044fa8166aba2d6b4829599f473689c63
tikhan1019 <tikhanlesandovski@gmail.com>
2020-02-13 11:06:47 +0100

    👷() Add lint rules and git hooks

--- truffle.js
+++ truffle.js
@@ -53,13 +53,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-	version: '0.5.10',
-	settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
+      version: '0.5.10'
     },
   },
 };
